fa6bd4982a6ab9f17655d81412067b21ec833b8e
isaaccollinsinfo-source <isaaccollins.info@gmail.com>
2026-01-31 15:46:50 -0800

    user interface attempt

---
+++ godot/ggj-2026/scenes/user_interface.gd
@@ -0,0 +1,13 @@
+
+extends CanvasLayer
+
+var score = 0
+var timer = 0
+# Function to update the label
+func update_score_label():
+	$ScoreLabel.text = "Score: " + str(score)
+
+# Call this function whenever the player collects an item
+func _on_score_increased(amount):
+	score += amount
+	update_score_label()
