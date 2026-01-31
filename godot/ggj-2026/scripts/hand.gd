7b7ba684c32a4c15c052593670f28c3bc32efb02
Devin Curry <devination3d@gmail.com>
2026-01-31 15:43:36 -0800

    draggable masks

--- godot/ggj-2026/scripts/hand.gd
+++ godot/ggj-2026/scripts/hand.gd
@@ -1,3 +1,4 @@
+class_name Hand
 extends Node2D
 
 @export var possible_masks: Array[Mask]  = []
@@ -10,4 +11,7 @@ func _ready() -> void:
 	mask_points = [$MaskPoint1, $MaskPoint2, $MaskPoint3]
 
 func _process(delta: float) -> void:
+	if dragging_mask:
+		dragging_mask.global_position = get_global_mouse_position()
+	#global_position = get_global_mouse_position()
 	pass
--- godot/ggj-2026/scripts/mask.gd
+++ godot/ggj-2026/scripts/mask.gd
@@ -3,12 +3,23 @@ class_name Mask
 extends Node2D
 
 enum Interests {THING_1 = 1, THING_2 = 2, THING_3 = 3}
+var hand:Hand
 
 @export var attribute: Array[Interests] = [] # TODO: use enum of interests
 
 func _ready() -> void:
-	pass
+	hand = %Hand
 
 func _process(delta: float) -> void:
-	#global_position = get_global_mouse_position()
+	# global_position = get_global_mouse_position()
 	pass
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+			#print(get_global_mouse_position(), shape_idx)
+			hand.dragging_mask = self
+		else:
+			hand.dragging_mask = null
+		
