4898c6257e87ccc7bf48b592ec604a891a9f6e2e
Devin Curry <devination3d@gmail.com>
2026-01-31 23:10:37 -0800

    masks can be applied to guests

--- godot/ggj-2026/scripts/guest.gd
+++ godot/ggj-2026/scripts/guest.gd
@@ -2,9 +2,15 @@
 class_name Guest
 extends Node2D
 
+var mask: Mask
 enum Interests {THING_1 = 1, THING_2 = 2, THING_3 = 3}
 
 @export var likes: Array[Interests] = []
 
 func _ready() -> void:
-	pass
+	mask = $Mask # get the child object named "Mask" at game start
+
+func wear_mask(new_mask:Mask):
+	print("Wearing mask: ", mask)
+	# TODO: delete mask
+	mask = new_mask
--- godot/ggj-2026/scripts/mask.gd
+++ godot/ggj-2026/scripts/mask.gd
@@ -3,12 +3,14 @@ class_name Mask
 extends Node2D
 
 enum Interests {THING_1 = 1, THING_2 = 2, THING_3 = 3}
-var hand:Hand
+var hand: Hand
+var area: Area2D
 
 @export var attribute: Array[Interests] = [] # TODO: use enum of interests
 
 func _ready() -> void:
 	hand = %Hand
+	area = $Area2D
 
 func _process(delta: float) -> void:
 	# global_position = get_global_mouse_position()
@@ -20,6 +22,14 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
 			#print(get_global_mouse_position(), shape_idx)
 			hand.dragging_mask = self
+		elif event.button_index == MOUSE_BUTTON_LEFT and event.is_released() and hand.dragging_mask == self:
+			hand.dragging_mask = null
+			apply_mask_to_guest()
 		else:
 			hand.dragging_mask = null
-		
+
+func apply_mask_to_guest():
+	var bodies = area.get_overlapping_areas()
+	for body in bodies:
+		var guest := body.get_parent() as Guest
+		guest.wear_mask(self)
