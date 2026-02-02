4a6bb6758df51288a8d5d78c61453916f70a26c5
Devin Curry <devination3d@gmail.com>
2026-02-01 18:07:14 -0800

    guests minge

--- godot/ggj-2026/scripts/guest.gd
+++ godot/ggj-2026/scripts/guest.gd
@@ -1,9 +1,10 @@
 # Masks have interests that modify guest interests and can be dragged between guests and the hand
 class_name Guest
-extends Node2D
+extends RigidBody2D
 
 var mask: Mask
 var hand: Hand
+var initial_force: float = 1.0
 
 @export var likes: Array[Mask.MaskTypes] = []
 
@@ -13,7 +14,7 @@ func _ready() -> void:
 	mask.is_draggable = false
 	init_likes()
 	hand.register_guest(self)
-	
+	init_movement()
 
 func init_likes():
 	# like a random thing if none specified
@@ -23,7 +24,13 @@ func init_likes():
 	# show that you like that thing
 	var like_displays = [$Heart/LikeMask1, $Heart/LikeMask2]
 	like_displays[likes[0]].set_visible(true)
-	
+
+# mingle with the other guests
+func init_movement():
+	# start movement toward center of screen
+	var center: Vector2 = get_viewport_rect().size / 2
+	var direction: Vector2 = center - global_position
+	self.apply_impulse(direction * initial_force)
 
 func wear_mask(new_mask:Mask):
 	# delete old mask
